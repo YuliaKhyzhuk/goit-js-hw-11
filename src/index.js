8bf6035cd3e2ae53e02be7c2c5ceeefaefa526ad
YuliaKhyzhuk <anastezy1@gmail.com>
2023-02-03 03:49:57 +0200

    Додані вихідні файли

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './css/styles.css';
+import './css/gallery.css';
+
+import { createCountryInfoCard } from './js/templates/gallery-cards.js';
+import Notiflix from 'notiflix';
+
+console.log('test');
