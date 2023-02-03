cfa019ea6ac785d7c1dfa969f3a916ae6715abd0
YuliaKhyzhuk <anastezy1@gmail.com>
2023-02-03 04:53:02 +0200

    Додане API

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import './css/styles.css';
 import './css/gallery.css';
-
-import { createCountryInfoCard } from './js/templates/gallery-cards.js';
+import { PixabayAPI } from './js/pixabay-api.js';
+import { createGalleryCards } from './js/templates/gallery-cards.js';
 import Notiflix from 'notiflix';
 
 console.log('test');
+
+const searchFormEl = document.querySelector('#search-form');
+const galleryListEl = document.querySelector('.js-gallery');
+const loadMoreBtnEl = document.querySelector('.js-load-more');
+
+const pixabayAPI = new PixabayAPI();
+console.log(pixabayAPI);
+
+const onSearchFormSubmit = async event => {
+  event.preventDefault();
+};
+
+const onLoadMoreBtnClick = async event => {};
+
+searchFormEl.addEventListener('submit', onSearchFormSubmit);
+loadMoreBtnEl.addEventListener('click', onLoadMoreBtnClick);
---
+++ src/js/pixabay-api.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+export class PixabayAPI {
+  static BASE_URL = 'https://pixabay.com/api/';
+  static API_KEY = '15090936-31f0088bbfe3b64ca1007e23b';
+
+  constructor() {
+    this.page = null;
+    this.query = null;
+  }
+
+  fetchPhotosByQuery() {
+    const searchParams = {
+      params: {
+        key: PixabayAPI.API_KEY,
+        q: this.query,
+        image_type: photo,
+        orientation: 'horizontal',
+        safesearch: true,
+        page: this.page,
+        per_page: 40,
+      },
+    };
+
+    return axios.get(`${PixabayAPI.BASE_URL}/search/photos`, searchParams);
+  }
+}
